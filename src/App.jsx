0e1e9e8438f697d1901a234c5709e0dde63b3b6b
tobymatthew <trobini18@gmail.com>
2021-07-19 06:36:46 +0100

    working on the sidebar

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import "./app.css";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Topbar/>
+
+     <div className="container">
+       <Sidebar/>
+       <Home/> 
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./home.css"
+export default function home() {
+    return (
+        <div className="home">
+         Home    
+        </div>
+    )
+}
